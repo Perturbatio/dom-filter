5e8550d61834083c0326502bba61369426851721
Kris Kelly <bertpotato@gmail.com>
2016-09-02 18:44:29 +0100

    added support for feedback messages added support for language based strings added simple string replacement templating

--- build/dom-filter-compiled.js
+++ build/dom-filter-compiled.js
@@ -5,23 +5,144 @@
  */
 
 var DomFilter = function () {
-	/**
-  *
-  * @param config
-  */
+	babelHelpers.createClass(DomFilter, [{
+		key: 'getLangStrings',
+
+
+		/**
+   * Return all strings for a given language
+   *
+   * @param {String} lang defaults to 'en'
+   * @returns {*}
+   */
+		value: function getLangStrings(lang) {
+			var me = this;
+			lang = lang || 'en';
+			if (typeof lang !== 'string' || !me.strings.hasOwnProperty(lang)) {
+				return {};
+			}
+			return me.strings[lang];
+		}
+
+		/**
+   *
+   * @param {Object} config
+   */
+
+	}, {
+		key: 'strings',
+
+		/**
+   *
+   * @returns {Object}
+   */
+		get: function get() {
+			var me = this;
+			if (typeof me._strings === 'undefined') {
+				me._strings = DomFilter._getDefaultLangStrings();
+			}
+			return me._strings;
+		}
+
+		/**
+   *
+   * @param {Object} value
+   */
+		,
+		set: function set(value) {
+			this._strings = value;
+		}
+
+		/**
+   *
+   * @returns {string}
+   */
+
+	}, {
+		key: 'defaultLanguage',
+		get: function get() {
+			var me = this;
+			if (typeof me._defaultLang !== 'string') {
+				me._defaultLang = 'en';
+			}
+			return me._defaultLang || '';
+		}
+
+		/**
+   *
+   * @param {string} value
+   */
+		,
+		set: function set(value) {
+			if (typeof value === 'string') {
+				this._defaultLang = value;
+			}
+		}
+
+		/**
+   *
+   * @returns {Element}
+   */
+
+	}, {
+		key: 'feedbackElement',
+		get: function get() {
+			return this.filterContainer.querySelector(this._config.feedbackElement);
+		}
+
+		/**
+   *
+   * @param {string|Element} value
+   */
+		,
+		set: function set(value) {
+			this._config.feedbackElement = value;
+		}
+
+		/**
+   * Get the default language strings for the class
+   *
+   * @returns {Object}
+   * @private
+   */
+
+	}], [{
+		key: '_getDefaultLangStrings',
+		value: function _getDefaultLangStrings() {
+			return {
+				'en': {
+					'feedback': {
+						'no_results': 'No results',
+						'default_feedback_message': ''
+					},
+					'input': {
+						'placeholder': 'Filter',
+						'label': 'Filter this list:'
+					}
+				}
+			};
+		}
+	}]);
 
 	function DomFilter(config) {
 		babelHelpers.classCallCheck(this, DomFilter);
 
 		var me = this;
+
 		me._config = config || {};
-		me._config.filterTemplate = me._config.filterTemplate || '<div class="element-filter"><input type="text" placeholder="Filter"></div>';
-		me._config.input = me._config.input || '.element-filter input';
+		me._initialConfig = me._config;
+
+		me._config.feedbackElement = me._config.feedbackElement || '.feedback';
+		me._config.containerClass = me._config.containerClass || 'element-filter';
+
+		me._config.filterTemplate = me._config.filterTemplate || '<div class="' + me._config.containerClass + '">' + '<label>{{input.label}}<input type="text" placeholder="{{input.placeholder}}"></label>' + '<div class="feedback"></div>' + '</div>';
+		me._config.inputElement = me._config.inputElement || '.element-filter input';
 		me._filterNodes = document.querySelectorAll(me._config.filterNodes);
-		me._hideFunction = config.hideFunction || DomFilter._fnHideNodes;
-		me._showFunction = config.showFunction || DomFilter._fnShowNodes;
-		me._matchFunction = config.matchFunction || DomFilter._fnMatchNode;
-		me.createInput();
+		me._hideFunction = me._config.hideFunction || DomFilter._fnHideNodes;
+		me._showFunction = me._config.showFunction || DomFilter._fnShowNodes;
+		me._matchFunction = me._config.matchFunction || DomFilter._fnMatchNode;
+
+		me.createFilterElement();
 		me.registerListeners();
 	}
 
@@ -34,13 +155,13 @@ var DomFilter = function () {
 		key: 'registerListeners',
 		value: function registerListeners() {
 			var me = this;
-			me._input.addEventListener('keypress', function (e) {
+			me.inputElement.addEventListener('keypress', function (e) {
 				return me._onFilterChange(e);
 			});
-			me._input.addEventListener('change', function (e) {
+			me.inputElement.addEventListener('change', function (e) {
 				return me._onFilterChange(e);
 			});
-			me._input.addEventListener('input', function (e) {
+			me.inputElement.addEventListener('input', function (e) {
 				return me._onFilterChange(e);
 			});
 		}
@@ -64,7 +185,7 @@ var DomFilter = function () {
    */
 		value: function _onFilterChange(e) {
 			var me = this,
-			    searchText = typeof me._input.value !== 'undefined' ? me._input.value.trim() : me._input.innerText.trim();
+			    searchText = typeof me.inputElement.value !== 'undefined' ? me.inputElement.value.trim() : me.inputElement.innerText.trim();
 			me._showFunction(me._filterNodes);
 			this.filter(searchText);
 		}
@@ -89,6 +210,26 @@ var DomFilter = function () {
 				unmatchedNodes = me._getUnmatchedNodes(me._filterNodes, matchedNodes);
 				me._showFunction(matchedNodes);
 				me._hideFunction(unmatchedNodes);
+				if (matchedNodes.length < 1) {
+					me.feedback(me._strings[me.defaultLanguage].feedback.no_results);
+				} else {
+					me.feedback(me._strings[me.defaultLanguage].feedback.default_feedback_message);
+				}
+			} else {
+				me.feedback(me._strings[me.defaultLanguage].feedback.default_feedback_message);
+			}
+		}
+
+		/**
+   * Add a message to the feeback element
+   * @param text
+   */
+
+	}, {
+		key: 'feedback',
+		value: function feedback(text) {
+			if (typeof this.feedbackElement !== 'undefined' && this.feedbackElement) {
+				this.feedbackElement.innerHTML = text;
 			}
 		}
 	}, {
@@ -110,10 +251,10 @@ var DomFilter = function () {
    */
 
 	}, {
-		key: 'createInput',
-		value: function createInput() {
+		key: 'createFilterElement',
+		value: function createFilterElement() {
 			var me = this,
-			    filterContainer = DomFilter._createNodes(me._config.filterTemplate),
+			    filterContainer = DomFilter._createNodes(me.replaceStrings(me._config.filterTemplate, me.getLangStrings())),
 			    appendToNode,
 			    beforeNode,
 			    afterNode;
@@ -129,16 +270,40 @@ var DomFilter = function () {
 				appendToNode.appendChild(filterContainer);
 			}
 
-			me._input = filterContainer.querySelector(me._config.input);
+			me.filterContainer = filterContainer;
+			me.inputElement = filterContainer.querySelector(me._config.inputElement);
 		}
 
 		/**
    *
-   * @param insertNode
-   * @param afterNode
+   * @param {Element} insertNode
+   * @param {Element} afterNode
    * @private
    */
 
+	}, {
+		key: 'replaceStrings',
+
+
+		/**
+   *
+   * @param {string} filterTemplate
+   * @param {object} strings
+   * @returns {string}
+   */
+		value: function replaceStrings(filterTemplate, strings) {
+			filterTemplate = filterTemplate || '';
+
+			var regex = /({{([A-Za-z]+[A-Za-z0-9]*?(\.(?=[A-Za-z])){0,1})+}})/ig;
+			return filterTemplate.replace(regex, function (match) {
+
+				match = match.replace('{{', '').replace('}}', ''); //ugly hack because I need to work on my regexes
+
+				return match.split('.').reduce(function (a, b) {
+					return a[b];
+				}, strings);
+			});
+		}
 	}], [{
 		key: '_fnShowNodes',
 		value: function _fnShowNodes(nodes) {
@@ -183,7 +348,7 @@ var DomFilter = function () {
 		value: function _insertAfter(insertNode, afterNode) {
 			var nextNode = afterNode.nextSibling;
 			if (typeof nextNode !== 'undefined') {
-				afterNode.parentNode.insertBefore(insertNode, afterNode.nextSibling);
+				afterNode.parentNode.insertBefore(insertNode, nextNode);
 			} else {
 				afterNode.parentNode.appendChild(insertNode);
 			}
@@ -192,7 +357,7 @@ var DomFilter = function () {
 		/**
    *
    * @param html
-   * @returns {*|Node}
+   * @returns {*|Element}
    * @private
    */
 
--- src/dom-filter.js
+++ src/dom-filter.js
@@ -4,18 +4,122 @@
 class DomFilter {
 	/**
 	 *
-	 * @param config
+	 * @returns {Object}
+	 */
+	get strings() {
+		var me = this;
+		if ( typeof me._strings === 'undefined' ) {
+			me._strings = DomFilter._getDefaultLangStrings();
+		}
+		return me._strings;
+	}
+
+	/**
+	 *
+	 * @param {Object} value
+	 */
+	set strings( value ) {
+		this._strings = value;
+	}
+
+	/**
+	 *
+	 * @returns {string}
+	 */
+	get defaultLanguage() {
+		var me = this;
+		if ( typeof me._defaultLang !== 'string' ) {
+			me._defaultLang = 'en';
+		}
+		return me._defaultLang || '';
+	}
+
+	/**
+	 *
+	 * @param {string} value
+	 */
+	set defaultLanguage( value ) {
+		if ( typeof value === 'string' ) {
+			this._defaultLang = value;
+		}
+	}
+
+	/**
+	 *
+	 * @returns {Element}
+	 */
+	get feedbackElement() {
+		return this.filterContainer.querySelector(this._config.feedbackElement);
+	}
+
+	/**
+	 *
+	 * @param {string|Element} value
+	 */
+	set feedbackElement( value ) {
+		this._config.feedbackElement = value;
+	}
+
+	/**
+	 * Get the default language strings for the class
+	 *
+	 * @returns {Object}
+	 * @private
+	 */
+	static _getDefaultLangStrings() {
+		return {
+			'en': {
+				'feedback': {
+					'no_results'              : 'No results',
+					'default_feedback_message': ''
+				},
+				'input'   : {
+					'placeholder': 'Filter',
+					'label'      : 'Filter this list:'
+				}
+			}
+		};
+	}
+
+	/**
+	 * Return all strings for a given language
+	 *
+	 * @param {String} lang defaults to 'en'
+	 * @returns {*}
+	 */
+	getLangStrings( lang ) {
+		var me = this;
+		lang = lang || 'en';
+		if ( typeof lang !== 'string' || !me.strings.hasOwnProperty(lang) ) {
+			return {};
+		}
+		return me.strings[lang];
+	}
+
+	/**
+	 *
+	 * @param {Object} config
 	 */
 	constructor( config ) {
 		var me = this;
+
 		me._config = config || {};
-		me._config.filterTemplate = me._config.filterTemplate || '<div class="element-filter"><input type="text" placeholder="Filter"></div>';
-		me._config.input = me._config.input || '.element-filter input';
+		me._initialConfig = me._config;
+
+		me._config.feedbackElement = me._config.feedbackElement || '.feedback';
+		me._config.containerClass = me._config.containerClass || 'element-filter';
+
+		me._config.filterTemplate = me._config.filterTemplate || '<div class="' + me._config.containerClass + '">' +
+			'<label>{{input.label}}<input type="text" placeholder="{{input.placeholder}}"></label>' +
+			'<div class="feedback"></div>' +
+			'</div>';
+		me._config.inputElement = me._config.inputElement || '.element-filter input';
 		me._filterNodes = document.querySelectorAll(me._config.filterNodes);
-		me._hideFunction = config.hideFunction || DomFilter._fnHideNodes;
-		me._showFunction = config.showFunction || DomFilter._fnShowNodes;
-		me._matchFunction = config.matchFunction || DomFilter._fnMatchNode;
-		me.createInput();
+		me._hideFunction = me._config.hideFunction || DomFilter._fnHideNodes;
+		me._showFunction = me._config.showFunction || DomFilter._fnShowNodes;
+		me._matchFunction = me._config.matchFunction || DomFilter._fnMatchNode;
+
+		me.createFilterElement();
 		me.registerListeners();
 	}
 
@@ -24,9 +128,9 @@ class DomFilter {
 	 */
 	registerListeners() {
 		var me = this;
-		me._input.addEventListener('keypress', ( e ) => me._onFilterChange(e));
-		me._input.addEventListener('change', ( e ) => me._onFilterChange(e));
-		me._input.addEventListener('input', ( e ) => me._onFilterChange(e));
+		me.inputElement.addEventListener('keypress', ( e ) => me._onFilterChange(e));
+		me.inputElement.addEventListener('change', ( e ) => me._onFilterChange(e));
+		me.inputElement.addEventListener('input', ( e ) => me._onFilterChange(e));
 	}
 
 	/**
@@ -76,7 +180,7 @@ class DomFilter {
 	 */
 	_onFilterChange( e ) {
 		var me = this,
-			searchText = (typeof me._input.value !== 'undefined') ? me._input.value.trim() : me._input.innerText.trim();
+			searchText = (typeof me.inputElement.value !== 'undefined') ? me.inputElement.value.trim() : me.inputElement.innerText.trim();
 		me._showFunction(me._filterNodes);
 		this.filter(searchText);
 
@@ -97,6 +201,23 @@ class DomFilter {
 			unmatchedNodes = me._getUnmatchedNodes(me._filterNodes, matchedNodes);
 			me._showFunction(matchedNodes);
 			me._hideFunction(unmatchedNodes);
+			if ( matchedNodes.length < 1 ) {
+				me.feedback(me._strings[me.defaultLanguage].feedback.no_results);
+			} else {
+				me.feedback(me._strings[me.defaultLanguage].feedback.default_feedback_message);
+			}
+		} else {
+			me.feedback(me._strings[me.defaultLanguage].feedback.default_feedback_message);
+		}
+	}
+
+	/**
+	 * Add a message to the feeback element
+	 * @param text
+	 */
+	feedback( text ) {
+		if (typeof this.feedbackElement !== 'undefined' && this.feedbackElement) {
+			this.feedbackElement.innerHTML = text;
 		}
 	}
 
@@ -115,9 +236,9 @@ class DomFilter {
 	/**
 	 *
 	 */
-	createInput() {
+	createFilterElement() {
 		var me = this,
-			filterContainer = DomFilter._createNodes(me._config.filterTemplate),
+			filterContainer = DomFilter._createNodes(me.replaceStrings(me._config.filterTemplate, me.getLangStrings())),
 			appendToNode,
 			beforeNode,
 			afterNode;
@@ -134,19 +255,21 @@ class DomFilter {
 			appendToNode.appendChild(filterContainer);
 		}
 
-		me._input = filterContainer.querySelector(me._config.input);
+		me.filterContainer = filterContainer;
+		me.inputElement = filterContainer.querySelector(me._config.inputElement);
 	}
+
 
 	/**
 	 *
-	 * @param insertNode
-	 * @param afterNode
+	 * @param {Element} insertNode
+	 * @param {Element} afterNode
 	 * @private
 	 */
 	static _insertAfter( insertNode, afterNode ) {
 		var nextNode = afterNode.nextSibling;
 		if ( typeof nextNode !== 'undefined' ) {
-			afterNode.parentNode.insertBefore(insertNode, afterNode.nextSibling);
+			afterNode.parentNode.insertBefore(insertNode, nextNode);
 		} else {
 			afterNode.parentNode.appendChild(insertNode);
 		}
@@ -155,12 +278,33 @@ class DomFilter {
 	/**
 	 *
 	 * @param html
-	 * @returns {*|Node}
+	 * @returns {*|Element}
 	 * @private
 	 */
 	static _createNodes( html ) {
 		var template = document.createElement('template');
 		template.innerHTML = html;
 		return template.content.firstChild;
+	}
+
+	/**
+	 *
+	 * @param {string} filterTemplate
+	 * @param {object} strings
+	 * @returns {string}
+	 */
+	replaceStrings( filterTemplate, strings ) {
+		filterTemplate = filterTemplate || '';
+
+		var regex = /({{([A-Za-z]+[A-Za-z0-9]*?(\.(?=[A-Za-z])){0,1})+}})/ig;
+		return filterTemplate.replace(regex, function ( match ) {
+
+			match = match.replace('{{', '').replace('}}', '');//ugly hack because I need to work on my regexes
+
+			return match.split('.').reduce(function ( a, b ) {
+				return a[b];
+			}, strings);
+		});
+
 	}
 }
